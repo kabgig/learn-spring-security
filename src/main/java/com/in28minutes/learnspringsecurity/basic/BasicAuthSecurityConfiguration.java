67aa9cb21d4f6a82ef26cd47e477a43d6ab93c42
Ansar Ismagilov <ansar89kab@bk.ru>
2023-08-14 19:20:26 +0800

    storing user credentials

--- src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -22,5 +26,17 @@ public class BasicAuthSecurityConfiguration {
         http.httpBasic(withDefaults());
         http.csrf(csrf -> csrf.disable());
         return http.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var user = User.withUsername("in28minutes")
+                .password("{noop}dummy")
+                .roles("USER")
+                .build();
+        var admin = User.withUsername("admin")
+                .password("{noop}dummy")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
